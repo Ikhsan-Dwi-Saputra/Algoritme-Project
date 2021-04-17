e9609c6770c629eaa91e6788e960d1f6054d2f87
Ikhsan-cybernetics <12050113500@students.uin-suska.ac.id>
2021-04-18 06:25:58 +0700

    I Love You

--- src/com/ikhsan/project/asdasdasd.java
+++ src/com/ikhsan/project/asdasdasd.java
@@ -1,4 +1,14 @@
 package com.ikhsan.project;
 
 public class asdasdasd {
+    public static void main(String[] args) {
+
+
+        int x = 1;
+        int y = 10;
+        while (x < y) {
+            System.out.println("I Love You");
+            x++;
+        }
+    }
 }
